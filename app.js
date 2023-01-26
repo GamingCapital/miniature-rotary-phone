089a915c9fa3a8f0d4bba281323ed9062b181214
GamingCapital <88299143+GamingCapital@users.noreply.github.com>
2023-01-26 07:17:30 +0000

    done

--- app.js
+++ app.js
@@ -22,10 +22,21 @@ io.on('connection', socket => {
     socket.on('new-user-joined', name => {
         console.log("New user", name);
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+        socket.broadcast.emit('user-joined', { name: name, onlineUsers: Object.keys(users).length });
     });
 
+    setInterval(function () {
+        socket.broadcast.emit('onlineUsers', { onlineUsers: Object.keys(users).length })
+    }, 50);
+
     socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: user[socket.id] })
+        console.log(`New message from ${users[socket.id]} : ${message}`)
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`${users[socket.id]} got disconnect!`)
+        socket.broadcast.emit('left', { name: users[socket.id], onlineUsers: Object.keys(users).length })
+        delete users[socket.id]
     });
 })
--- public/js/client.js
+++ public/js/client.js
@@ -1,39 +1,64 @@
 const socket = io();
 
 const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp')
-const messageContainer = document.querySelector(".container")
-var audio = new Audio('ting.mp3');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+const onlineStatus = document.querySelector('.online');
+var receive = new Audio('receive.mp3');
+var send = new Audio('send.mp3');
+var join = new Audio('join.mp3');
+var leave = new Audio('leave.mp3');
 
-const append = (message, position) => {
+const append = (message, position, userType=null) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
-    if (position == 'left') {
-        audio.play();
+    switch (position) {
+        case 'left':
+            receive.play();
+        case 'right':
+            send.play();
+        case 'center':
+            if (userType == "join") {
+                join.play();
+            } else {
+                leave.play();
+            }
     }
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
-    append(`You: ${message}`, 'right');
-    socket.emit('send', message);
-    messageInput.value = ''
+    if (message) {
+        append(`You: ${message}`, 'right');
+        socket.emit('send', message);
+        messageInput.value = ''
+    }
 })
 
 const name = prompt("Enter your name to join");
 
 socket.emit('new-user-joined', name);
-socket.on('user-joined', name => {
-    append(`${name} joined the chat`, 'right')
-})
-socket.on('receive', data => {
-    append(`${data.name}:${data.message}`, 'left')
+
+onlineStatus.innerText = `${parseInt(onlineStatus.innerText.replace(' Users online', ''))+1} Users online`
+
+socket.on('user-joined', data => {
+    append(`${data.name} joined the chat`, 'center', 'join');
+    onlineStatus.innerText = `${data.onlineUsers} Users online`
 })
 
-socket.on(' left ', name => {
-    append(`${name} left the chat` , 'left')
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('onlineUsers', data => {
+    onlineStatus.innerText = `${data.onlineUsers} Users online`
+})
+
+socket.on('left', data => {
+    append(`${data.name} left the chat`, 'center', 'leave')
+    onlineStatus.innerText = `${data.onlineUsers} Users online`
 })
